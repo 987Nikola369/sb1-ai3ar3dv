4a5c2558f3afac7450e47518814b19a41e53fca0
987Nikola369 <109827635+987Nikola369@users.noreply.github.com>
2025-02-07 15:59:50 +0000

    del

--- README.md
+++
@@ -1,3 +0,0 @@
-# sb1-ai3ar3dv
-
-[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/987Nikola369/sb1-ai3ar3dv)
--- src/lib/storage/index.ts
+++
@@ -1,9 +0,0 @@
-import { createStorageAdapter } from './adapter';
-import { LocalStorageAdapter } from './local';
-
-// Create an adapter that can be switched between local and Supabase
-export const storage = createStorageAdapter(new LocalStorageAdapter());
-
-// Export types
-export type { StorageAdapter } from './adapter';
-export type { UploadResult } from './types';
